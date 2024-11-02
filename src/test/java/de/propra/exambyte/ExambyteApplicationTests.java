38f98c87020389c9bff9fb847e035ef43a7a0aad
HanmaDevin <devin.cardosa@proton.me>
2024-11-02 09:33:13 +0100

    Initial commit

---
+++ src/test/java/de/propra/exambyte/ExambyteApplicationTests.java
@@ -0,0 +1,13 @@
+package de.propra.exambyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExambyteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
