a44068a2e72e873319914968c61ab72681293206
HanmaDevin <devinbc14@gmail.com>
2024-12-16 17:55:25 +0100

    new button appears if logged in with github

--- src/main/java/de/propra/exambyte/WebController.java
+++ src/main/java/de/propra/exambyte/WebController.java
@@ -1,12 +1,18 @@
 package de.propra.exambyte;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
     @GetMapping("/")
-    public String home() {
+    public String home(OAuth2AuthenticationToken oauth, Model model) {
+        if (oauth != null) {
+            String clientId = oauth.getAuthorizedClientRegistrationId();
+            model.addAttribute("id", clientId);
+        }
         return "public/home";
     }
 
