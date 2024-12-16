9215b4c6200f0b45dfea3943f074597a6b26cad5
HanmaDevin <devinbc14@gmail.com>
2024-12-16 16:54:12 +0100

    tests pass now

--- src/test/java/de/propra/exambyte/WebTest.java
+++ src/test/java/de/propra/exambyte/WebTest.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -18,8 +18,9 @@ public class WebTest {
 
     @Test
     @DisplayName("Die Startseite ist unter '/' erreichbar")
+    @WithMockUser()
     void test_home_page() throws Exception {
-        MvcResult result = mvc.perform(get("/"))
+        mvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andReturn();
     }
@@ -27,15 +28,16 @@ public class WebTest {
     @Test
     @DisplayName("Die Login-Seite ist unter '/login' erreichbar")
     void test_login_page() throws Exception {
-        MvcResult result = mvc.perform(get("/login"))
+        mvc.perform(get("/login"))
                 .andExpect(status().isOk())
                 .andReturn();
     }
 
     @Test
     @DisplayName("Die Registrierungsseite ist unter '/register' erreichbar")
+    @WithMockUser()
     void test_register_page() throws Exception {
-        MvcResult result = mvc.perform(get("/register"))
+        mvc.perform(get("/register"))
                 .andExpect(status().isOk())
                 .andReturn();
     }
