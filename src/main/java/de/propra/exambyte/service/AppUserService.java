be5cc0a8d4ace9bfa9eda3b50435fe7567c9bddd
HanmaDevin <devinbc14@gmail.com>
2024-12-17 11:59:15 +0100

    moved Service class to service package

--- src/main/java/de/propra/exambyte/service/AppUserService.java
+++ src/main/java/de/propra/exambyte/service/AppUserService.java
@@ -1,4 +1,4 @@
-package de.propra.exambyte;
+package de.propra.exambyte.service;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
