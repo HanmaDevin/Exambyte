56a73b2f0f84b028e24507b353482a97045aa9e7
HanmaDevin <devinbc14@gmail.com>
2024-12-17 11:48:39 +0100

    added first concept of a MCTest Objekt

---
+++ src/main/java/de/propra/exambyte/test_typen/MCTest.java
@@ -0,0 +1,23 @@
+package de.propra.exambyte.test_typen;
+
+import java.util.Set;
+
+public record MCTest(String aufgabenstellung, Set<String> antworten, int punktzahl, Set<String> loesung, String erklearung) {
+    public MCTest {
+        if (aufgabenstellung.isBlank()) {
+            throw new IllegalArgumentException("Aufgabenstellung darf nicht leer sein");
+        }
+        if (antworten.isEmpty()) {
+            throw new IllegalArgumentException("Antworten dürfen nicht leer sein");
+        }
+        if (punktzahl < 0) {
+            throw new IllegalArgumentException("Punktzahl darf nicht negativ sein");
+        }
+        if (loesung.isEmpty()) {
+            throw new IllegalArgumentException("Lösung darf nicht leer sein");
+        }
+        if (erklearung.isBlank()) {
+            throw new IllegalArgumentException("Erklärung darf nicht leer sein");
+        }
+    }
+}
