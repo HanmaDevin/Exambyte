7f25f9b13513cb6723057b4494181687e85b50cb
IliasAlhandiui <IliasAlhandiui@gmail.com>
2024-12-20 22:35:56 +0100

    Reworked the Role-System and introduces a config file to change the assigned users

---
+++ src/main/java/de/propra/exambyte/config/ConfigLoader.java
@@ -0,0 +1,54 @@
+package de.propra.exambyte.config;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class ConfigLoader {
+
+    @Value("${config.roles}")
+    private String configRoles;
+    private Map<String, Object> configData;
+
+    public Map<String, Object> getConfigData() {
+        if (configData == null) {
+            loadConfig();
+        }
+        return configData;
+    }
+
+    void loadConfig() {
+        try {
+            // Load config from file
+            String content = Files.readString(Paths.get(configRoles));
+            ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+            configData = mapper.readValue(content, new TypeReference<>() {
+            });
+        } catch (Exception e) {
+            // Handle exception
+            throw new RuntimeException("Error loading config file", e);
+        }
+    }
+
+    public String getRole(String username) {
+        configData = getConfigData();
+        List<Map<String, String>> users = (List<Map<String, String>>) configData.get("users");
+
+        for (Map<String, String> user : users) {
+            if (user.get("username").equals(username)) {
+                return user.get("role");
+            }
+        }
+        return "student";
+    }
+
+
+}
