620c5854e4b56762929c47ed2aaa9154304cb7b7
IliasAlhandiui <IliasAlhandiui@gmail.com>
2024-12-20 23:36:59 +0100

    Added a Temp Dashboard to view current Role for debugging purpose

--- src/main/java/de/propra/exambyte/WebController.java
+++ src/main/java/de/propra/exambyte/WebController.java
@@ -1,9 +1,15 @@
 package de.propra.exambyte;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
 
 @Controller
 public class WebController {
@@ -25,4 +31,17 @@ public class WebController {
     public String register() {
         return "public/register";
     }
+
+    // Temp dashboard for debugging
+    @GetMapping("/dashboard")
+    public String showDashboard(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // Extract roles
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        model.addAttribute("roles", authorities);
+
+        return "public/organizer";
+    }
 }
+
