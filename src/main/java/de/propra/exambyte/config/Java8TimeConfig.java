fc6b9f1bb697453ac89f127a349ffbcb68a1b301
IliasAlhandiui <IliasAlhandiui@gmail.com>
2024-12-22 00:08:05 +0100

    Moved Java8Time to own Config File

---
+++ src/main/java/de/propra/exambyte/config/Java8TimeConfig.java
@@ -0,0 +1,13 @@
+package de.propra.exambyte.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+
+@Configuration
+public class Java8TimeConfig {
+    @Bean
+    public Java8TimeDialect java8TimeDialect() {
+        return new Java8TimeDialect();
+    }
+}
