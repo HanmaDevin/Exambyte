daacae732b3eb2f353653942389ec401f94a282f
HanmaDevin <devinbc14@gmail.com>
2024-12-23 16:38:49 +0100

    added exception handling for free text questions errors

--- src/main/java/de/propra/exambyte/handler/TestControllerExceptionHandler.java
+++ src/main/java/de/propra/exambyte/handler/TestControllerExceptionHandler.java
@@ -1,7 +1,10 @@
 package de.propra.exambyte.handler;
 
+import de.propra.exambyte.dto.FreeTextQuestionDto;
 import de.propra.exambyte.dto.TestDto;
-import de.propra.exambyte.exception.WrongDateInput;
+import de.propra.exambyte.exception.EmptyInputException;
+import de.propra.exambyte.exception.LowerThanZeroException;
+import de.propra.exambyte.exception.WrongDateInputException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,10 +13,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class TestControllerExceptionHandler {
 
 
-    @ExceptionHandler(WrongDateInput.class)
+    @ExceptionHandler(WrongDateInputException.class)
     public String handleWrongDateInput(Exception e, Model model) {
         model.addAttribute("error", e.getMessage());
         model.addAttribute("testDto", new TestDto());
         return "test-form";
     }
+
+    @ExceptionHandler(LowerThanZeroException.class)
+    public String handleLowerThanZeroException(Exception e, Model model) {
+        model.addAttribute("error", e.getMessage());
+        model.addAttribute("freeTextQuestionDto", new FreeTextQuestionDto());
+        return "free-text-question-form";
+    }
+
+    @ExceptionHandler(EmptyInputException.class)
+    public String handleEmptyInputException(Exception e, Model model) {
+        model.addAttribute("error", e.getMessage());
+        model.addAttribute("freeTextQuestionDto", new FreeTextQuestionDto());
+        return "free-text-question-form";
+    }
 }
+
