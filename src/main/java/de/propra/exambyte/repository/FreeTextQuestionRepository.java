d165aea97f790388ae24deb0dc8660ec4fde5a34
HanmaDevin <devinbc14@gmail.com>
2024-12-23 17:18:38 +0100

    added FreeTextQuestionRepository

--- src/main/java/de/propra/exambyte/repository/FreeTextQuestionRepository.java
+++ src/main/java/de/propra/exambyte/repository/FreeTextQuestionRepository.java
@@ -2,7 +2,9 @@ package de.propra.exambyte.repository;
 
 import de.propra.exambyte.model.FreeTextQuestion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FreeTextQuestionRepository extends JpaRepository<FreeTextQuestion, Long> {
     FreeTextQuestion getFreeTextQuestionById(Long id);
 }
