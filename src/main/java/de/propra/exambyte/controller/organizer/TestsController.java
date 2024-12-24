0acc27c3e6538a33e70c75f1c9ee679ead38a857
HanmaDevin <devinbc14@gmail.com>
2024-12-24 17:55:37 +0100

    added success message when creating a test

--- src/main/java/de/propra/exambyte/controller/organizer/TestsController.java
+++ src/main/java/de/propra/exambyte/controller/organizer/TestsController.java
@@ -2,12 +2,12 @@ package de.propra.exambyte.controller.organizer;
 
 import de.propra.exambyte.dto.TestDto;
 import de.propra.exambyte.model.Test;
-import de.propra.exambyte.service.FreeTextQuestionService;
 import de.propra.exambyte.service.TestService;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -16,11 +16,9 @@ import org.springframework.web.bind.annotation.*;
 public class TestsController {
 
     private final TestService testService;
-    private final FreeTextQuestionService freeTextQuestionService;
 
-    public TestsController(TestService testService, FreeTextQuestionService freeTextQuestionService) {
+    public TestsController(TestService testService) {
         this.testService = testService;
-        this.freeTextQuestionService = freeTextQuestionService;
     }
 
     @GetMapping
@@ -36,8 +34,9 @@ public class TestsController {
     }
 
     @PostMapping("/new")
-    public String createTest(@ModelAttribute TestDto testDto) {
+    public String createTest(@ModelAttribute TestDto testDto, RedirectAttributes redirectAttributes) {
         Test createdTest = testService.createTest(testDto);
+        redirectAttributes.addFlashAttribute("createdTest", createdTest);
         System.out.println(createdTest.toString());
         return "redirect:/organizer/tests";
     }
