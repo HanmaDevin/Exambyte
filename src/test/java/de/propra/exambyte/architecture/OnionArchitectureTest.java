0a9113b675c7e829f7072a20e0473a652a3189c0
HanmaDevin <devinbc14@gmail.com>
2025-01-05 16:29:58 +0100

    added architecture.puml to also check communications, still needs to be filled completely

--- src/test/java/de/propra/exambyte/architecture/OnionArchitectureTest.java
+++ src/test/java/de/propra/exambyte/architecture/OnionArchitectureTest.java
@@ -7,7 +7,12 @@ import com.tngtech.archunit.lang.ArchRule;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.net.URL;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.library.Architectures.onionArchitecture;
+import static com.tngtech.archunit.library.plantuml.rules.PlantUmlArchCondition.Configuration.consideringOnlyDependenciesInDiagram;
+import static com.tngtech.archunit.library.plantuml.rules.PlantUmlArchCondition.adhereToPlantUmlDiagram;
 
 public class OnionArchitectureTest {
 
@@ -24,6 +29,16 @@ public class OnionArchitectureTest {
                 //.adapter("web", "mwst.web")
                 //.adapter("persistence", "mwst.katalog")
                 //.adapter("kalender", "mwst.kalender");
+        rule.check(klassen);
+    }
+
+    @Test
+    @DisplayName("Die Architektur ist wie im UML Diagramm dargestellt")
+    void test_(){
+        // PlantUML-Datei liegt unter src/test/resources
+        URL uml = getClass().getResource("/architecture.puml");
+        ArchRule rule = classes().should(adhereToPlantUmlDiagram(
+                uml,consideringOnlyDependenciesInDiagram()));
         rule.check(klassen);
     }
 }
